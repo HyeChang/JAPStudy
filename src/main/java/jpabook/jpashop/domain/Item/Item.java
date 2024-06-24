1f82d9ade6bf9a1b2aa9bbba8a3cbc7ed22561fc
HyeChang <keygenerator@naver.com>
2024-06-24 15:33:53 +0900

    feat.상품 개수 증가, 감소 비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/Item/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.ManyToMany;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,5 +32,28 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     /* entity안에 비즈니스 로직을 넣는게 좋다(데이터를 가지고 있는 곳에서 로직이 동작=응집도 고려)
+     */
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     * 변경해야 할 일이있으면 'setter' 이 아닌 비즈니스 로직으로 변경해야 좋다
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
